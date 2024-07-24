987487319057cbf9b9615d89bf3b00978e59e978
Jay <sspal@yahoo.com>
2024-07-24 17:47:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,53 +8,53 @@ from dotenv import load_dotenv
 import os
 
 class ChatBot():
-  load_dotenv()
-  loader = TextLoader('./horoscope.txt')
-  documents = loader.load()
-  text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
-  docs = text_splitter.split_documents(documents)
+    def __init__(self):
+        load_dotenv()
+        self.loader = TextLoader('./horoscope.txt')
+        self.documents = self.loader.load()
+        self.text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
+        self.docs = self.text_splitter.split_documents(self.documents)
 
-  embeddings = HuggingFaceEmbeddings()
+        self.embeddings = HuggingFaceEmbeddings()
 
-  pinecone.init(
-      api_key= os.getenv('PINECONE_API_KEY'),
-      environment='gcp-starter'
-  )
+        pinecone.init(
+            api_key=os.getenv('PINECONE_API_KEY'),
+            environment='gcp-starter'
+        )
 
-  index_name = "langchain-demo"
+        self.index_name = "langchain-demo"
 
-  if index_name not in pinecone.list_indexes():
-    pinecone.create_index(name=index_name, metric="cosine", dimension=768)
-    docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-  else:
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+        if self.index_name not in pinecone.list_indexes():
+            pinecone.create_index(name=self.index_name, metric="cosine", dimension=768)
+            self.docsearch = Pinecone.from_documents(self.docs, self.embeddings, index_name=self.index_name)
+        else:
+            self.docsearch = Pinecone.from_existing_index(self.index_name, self.embeddings)
 
-  repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-  llm = HuggingFaceHub(
-      repo_id=repo_id, model_kwargs={"temperature": 0.8, "top_p": 0.8, "top_k": 50}, huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
-  )
+        self.repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+        self.llm = HuggingFaceHub(
+            repo_id=self.repo_id,
+            model_kwargs={"temperature": 0.8, "top_p": 0.8, "top_k": 50},
+            huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
+        )
 
-  from langchain import PromptTemplate
+        from langchain import PromptTemplate
 
-  template = """
-  You are a seer. These Human will ask you a questions about Dev Ops. Use following piece of context to answer the question. 
-  If you don't know the answer, just say you don't know. 
-  You answer with short and concise answer, no longer than2 sentences.
+        self.template = """
+        Welcome to Laitkor Consultancy Services, I am tying to provide answer for your queries about Devops.
 
-  Context: {context}
-  Question: {question}
-  Answer: 
+        Context: {context}
+        Question: {question}
+        Answer:
+        """
 
-  """
+        self.prompt = PromptTemplate(template=self.template, input_variables=["context", "question"])
 
-  prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+        from langchain.schema.runnable import RunnablePassthrough
+        from langchain.schema.output_parser import StrOutputParser
 
-  from langchain.schema.runnable import RunnablePassthrough
-  from langchain.schema.output_parser import StrOutputParser
-
-  rag_chain = (
-    {"context": docsearch.as_retriever(),  "question": RunnablePassthrough()} 
-    | prompt 
-    | llm
-    | StrOutputParser() 
-  )
+        self.rag_chain = (
+            {"context": self.docsearch.as_retriever(), "question": RunnablePassthrough()}
+            | self.prompt
+            | self.llm
+            | StrOutputParser()
+        )
